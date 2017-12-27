4e00deed3d267ead212831b3b4e8bec6981137c3
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-12-27 06:54:20 +0000

    add comments

--- lib/nem/endpoint/block.rb
+++ lib/nem/endpoint/block.rb
@@ -3,6 +3,7 @@ module Nem
     class Block < Nem::Endpoint::Base
       # @param hash
       # @return [Nem::Model::Block]
+      # @see https://nemproject.github.io/#
       def find(hash)
         Nem::Model::Block.new_from_block request!(:get, '/block/get', blockHash: hash)
       end
@@ -11,6 +12,7 @@ module Nem
 
       # @param height
       # @return [Nem::Model::Block]
+      # @see https://nemproject.github.io/#getting-a-block-with-a-given-height
       def at_public(height)
         Nem::Model::Block.new_from_block request!(:post, '/block/at/public', height: height)
       end
