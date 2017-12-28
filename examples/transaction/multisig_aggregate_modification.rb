025871e4e1933254699c04f46195a42ce91bc25d
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-12-28 21:23:41 +0900

    remove needless line

--- examples/transaction/multisig_aggregate_modification.rb
+++ examples/transaction/multisig_aggregate_modification.rb
@@ -7,12 +7,10 @@ Nem.logger.level = Logger::DEBUG
 M_PRIVATE_KEY = '00f077782658ae91b77f238ba5fcd7ef110564b5c189072e4d4590d9b17f9d76f3'
 
 # cosignatory(A)
-# A_PRIVATE_KEY = '4ce5c8f9fce571db0d9ac1adf00b8d3ba0f078ed40835fd3d730a2f24b834214'
 A_PUBLIC_KEY  = 'be2ba9cb15a547110d511a4d43c0482fbb584d78781abac01fb053d18f4a0033'
 
 kp = Nem::Keypair.new(M_PRIVATE_KEY)
 node = Nem::Node.new(host: 'bigalice2.nem.ninja')
-node = Nem::Node.new
 tx_endpoint = Nem::Endpoint::Transaction.new(node)
 
 msig_cosignatories = [
