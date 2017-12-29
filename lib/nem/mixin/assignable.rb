3983f876b9686aca71efe43ceb3b1719d28a0f6d
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-12-29 01:55:06 +0000

    add debugging

--- lib/nem/mixin/assignable.rb
+++ lib/nem/mixin/assignable.rb
@@ -4,9 +4,8 @@ module Nem
       def initialize(attributes = {})
         attributes.each do |k, v|
           instance_variable_set("@#{k.to_s}", v) if respond_to?("#{k.to_s}")
-          # NOTE: debugging
-          if Nem.debug && !respond_to?("#{k.to_s}")
-            Nem.logger.warn "Detected unknown key-value. #{k} => #{v} on #{self.class.name}"
+          if !respond_to?("#{k.to_s}")
+            Nem.logger.warn "Detected unknown key-value. #{k} => #{v} while assigning #{self.class.name}"
           end
         end if attributes
         yield self if block_given?
