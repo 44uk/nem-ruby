33704db499bec6192b10ccb409543d27ee827593
Yoshiyuki Ieyama <yukku0423@gmail.com>
2018-03-24 00:02:34 +0900

    deserialize multisig transfer

--- spec/nem/util/deserializer_spec.rb
+++ spec/nem/util/deserializer_spec.rb
@@ -23,11 +23,33 @@ describe Nem::Util::Deserializer do
       it { expect(subject).to match a_hash_including(tx) }
 
       context 'and multisig' do
-        let(:data) { '' }
+        let(:data) { '04100000010000987fcd9d0520000000cc63b4dcdec745417043c3fa0992ec3a1695461a26d90264744648abbd5caa0df0490200000000008fdb9d058600000001010000010000987fcd9d05200000003061144f1096de8b71178dfbc588069e30310112af57afe5fce87dbb828bbe8ba0860100000000008fdb9d05280000005441574b4a5455503444574b4c444b4b53353334545950364733323443424e4d584b424134583742809698000000000012000000010000000a000000476f6f64206c75636b21' }
         let(:tx) do
+          {
+            type: 4100,
+            version: -1744830463,
+            signer: 'cc63b4dcdec745417043c3fa0992ec3a1695461a26d90264744648abbd5caa0d',
+            timeStamp: 94227839,
+            deadline: 94231439,
+            fee: 150_000,
+            otherTrans: {
+              type: 257,
+              version: -1744830463,
+              signer: '3061144f1096de8b71178dfbc588069e30310112af57afe5fce87dbb828bbe8b',
+              timeStamp: 94227839,
+              deadline: 94231439,
+              recipient: 'TAWKJTUP4DWKLDKKS534TYP6G324CBNMXKBA4X7B',
+              amount: 10_000_000,
+              fee: 100_000,
+              message: {
+                type: 1,
+                payload: 'Good luck!'
+              }
+            }
+          }
         end
 
-        xit { expect(subject).to match a_hash_including(tx) }
+        it { expect(subject).to match a_hash_including(tx) }
       end
     end
 
