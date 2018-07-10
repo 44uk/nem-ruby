ecc1d26cc00d99a847195ed786d517f885440cf0
Yoshiyuki Ieyama <yukku0423@gmail.com>
2018-07-11 00:08:56 +0900

    add spec for Nem::Transaction::Transfer

--- spec/nem/transaction/transfer_spec.rb
+++ spec/nem/transaction/transfer_spec.rb
@@ -17,6 +17,10 @@ describe Nem::Transaction::Transfer do
     )
   }
 
+  describe 'version' do
+    it { expect(subject.version).to eq 0x98000001 }
+  end
+
   describe 'transfer?' do
     it { expect(subject.transfer?).to be true }
     it { expect(subject.multisig?).to be false }
@@ -31,5 +35,42 @@ describe Nem::Transaction::Transfer do
   end
 
   context 'has mosaics' do
+    let(:mosaics) { [
+      Nem::Mosaic::Xem.new(1),
+      Nem::Mosaic::EcobitEco.new(100)
+    ] }
+
+    it { expect(subject.has_mosaics?).to be true }
+    it { expect(subject.mosaics.size).to eq 2 }
+  end
+
+  describe '#to_hash' do
+    it { expect(subject.to_hash).to match a_hash_including(
+      amount: 1_000_000,
+      recipient: 'TA4TX6U5HG2MROAESH2JE5524T4ZOY2EQKQ6ELHF',
+      message: { payload: '', type: 1 }
+    ) }
+
+    context 'has decimal amount' do
+      let(:amount) { 0.001 }
+
+      it { expect(subject.to_hash).to match a_hash_including(
+        amount: 1_000
+      ) }
+
+      it { expect(subject.to_hash[:amount]).to be_a(Integer) }
+    end
+
+    context 'has decimal quantity mosaics' do
+      let(:mosaics) { [
+        Nem::Mosaic::DimToken.new(0.001)
+      ] }
+
+      it { expect(subject.to_hash).to match a_hash_including(
+        mosaics: [{ mosaicId: { namespaceId: 'dim', name: 'token' }, quantity: 1000 }]
+      ) }
+
+      it { expect(subject.to_hash[:mosaics].first[:quantity]).to be_a(Integer) }
+    end
   end
 end
